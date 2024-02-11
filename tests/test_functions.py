91b034547bc53254a922ec38b9ce46d2f5333c91
Robert Schmidt <allthingscoding21th@gmail.com>
2024-02-11 18:24:50 +0100

    Fix formattting

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -16,6 +16,7 @@ def test_my_throw_function_throw():
     with pytest.raises(ValueError):
         example.my_throw_function(15)
 
+
 def test_my_multiply_function():
     result = example.my_multiply_function(2, 5)
     assert result == 10
