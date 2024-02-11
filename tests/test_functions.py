198f81868e423031ac8ce155ad07d7dc03d5904b
Robert Schmidt <allthingscoding21th@gmail.com>
2024-02-11 17:49:51 +0100

    Fix test

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -2,6 +2,8 @@ from github_actions_python import example
 import pytest
 
 
+
+
 def test_my_add_function():
     result = example.my_add_function(5, 10)
     assert result == 15
@@ -13,4 +15,5 @@ def test_my_throw_function_not_throw():
 
 
 def test_my_throw_function_throw():
-    example.my_throw_function(15)
+    with pytest.raises(ValueError):
+        example.my_throw_function(15)
