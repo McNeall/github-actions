8f0878879aa70b645669395c9281e5101630729c
Robert Schmidt <allthingscoding21th@gmail.com>
2024-02-11 17:28:54 +0100

    Adding code

---
+++ src/github_actions_python/example.py
@@ -0,0 +1,8 @@
+def my_add_function(a: int, b: int) -> int:
+    return a + b
+
+
+def my_throw_function(a: int) -> int:
+    if a >                10:
+        raise ValueError("a is greater than 10")
+    return a * 2
---
+++ tests/test_functions.py
@@ -0,0 +1,16 @@
+from github_actions_python import example
+import pytest
+
+
+def test_my_add_function():
+    result = example.my_add_function(5, 10)
+    assert result == 15
+
+
+def test_my_throw_function_not_throw():
+    result = example.my_throw_function(5)
+    assert result == 10
+
+
+def test_my_throw_function_throw():
+    example.my_throw_function(15)
